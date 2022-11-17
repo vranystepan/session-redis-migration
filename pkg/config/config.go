5645b6513be4f731a14d6aa6a69b52e1c9356b09
Stepan Vrany <stepan@vrany.dev>
2022-11-17 15:23:54 +0100

    feat: add some defaults to config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"time"
@@ -24,10 +25,19 @@ func New() (*Config, error) {
 	targetTTLEnv := os.Getenv("TARGET_TTL")
 	filePatternEnv := os.Getenv("FILE_PATTERN")
 
-	// parse values
+	// parse values and add some default values for the empty ones
+	if redisHostEnv == "" {
+		log.Println("defaulting host to localhost")
+		redisHostEnv = "localhost"
+	}
+
 	redisPort, err := strconv.Atoi(redisPortEnv)
 	if err != nil {
 		return nil, fmt.Errorf("could not parse REDIS_PORT: %s", err)
+	}
+	if redisPort == 0 {
+		log.Println("defaulting to port 6379")
+		redisPort = 6379
 	}
 
 	redisDB, err := strconv.Atoi(redisDBEnv)
@@ -35,6 +45,10 @@ func New() (*Config, error) {
 		return nil, fmt.Errorf("could not parse REDIS_DB: %s", err)
 	}
 
+	if targetTTLEnv == "" {
+		log.Println("defaulting ttl to 14 days")
+		targetTTLEnv = "336h"
+	}
 	targetTTL, err := time.ParseDuration(targetTTLEnv)
 	if err != nil {
 		return nil, fmt.Errorf("could not parse TARGET_TTL: %s", err)
